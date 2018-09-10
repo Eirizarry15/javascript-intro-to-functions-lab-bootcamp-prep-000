65d37026beb629ad59cdb6e554a8146af65ba0b1
Edgar Irizarry <eirizarry15@yahoo.com>
2018-09-10 20:25:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log ('HELLO');
+}
+
+function logWhisper(string){
+
+  console.log ('hello');
+}
+
+
+  function sayHiToGrandma(string){
+  return string.toLowerCase()
+  console.log ('I cant hear you!')
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
